49c8721a7dd06d9f555d9e76eb59708173c7cda6
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2020-09-09 13:02:57 +0100

    committedd

--- main.js
+++ main.js
@@ -16,15 +16,199 @@ alert(myInput); */
 // DAY 3
 // IF-ELSE STATEMENT
 // AIRTIME-SELF, AIRTIME-OTHERS, DATA, TRANSFER
-let myAccount = "Welcome to Lexitar Services Limited \n 1. Airtime-self \n 2. Airtime-others \n 3. Data \n 4. Transfer";
-variable = parseInt(prompt(myAccount));
-console.log(typeof (variable));
-/*let amount = "Amount \n 1. 100 \n 2. 200 \n 3. 300";
-prompt(amount);
-let ans = confirm("Are you sure?");*/
+let myNetwork = "Welcome to Lexitar Services Limited \n Select your network \n 1. Airtel \n 2. MTN \n 3. GLO \n 4. 9mobile";
+let myNetworks = parseInt(prompt(myNetwork));
 
-if (variable === 1) {
-  prompt("Javascript is good");
-} else {
-
+if (myNetworks === 1) {
+  let myAccount = "1. Airtime-self \n 2. Airtime-others \n 3. Data \n 4. Transfer";
+  let myAccounts = parseInt(prompt(myAccount));
+  if (myAccounts === 1) {
+    let myAmount = prompt("Enter Amount");
+    if (myAmount <= 500) {
+      let myData = " 1. 200mb \n 2. 400mb \n 3. 600mb \n 4. 800mb";
+      let myDatas = parseInt(prompt(myData));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    } else if (myAmount > 500) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 2) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 3) {
+    let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+    let myData2s = parseInt(prompt(myData2));
+    if (myData2s = Number) {
+      let final = confirm("Are you sure you want to complete this subscription");
+      alert("Successful Subscription, Thanks!");
+    }
+  } else if (myAccounts === 4) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  }
+} else if (myNetworks === 2) {
+  let myAccount = "1. Airtime-self \n 2. Airtime-others \n 3. Data \n 4. Transfer";
+  let myAccounts = parseInt(prompt(myAccount));
+  if (myAccounts === 1) {
+    let myAmount = prompt("Enter Amount");
+    if (myAmount <= 500) {
+      let myData = " 1. 200mb \n 2. 400mb \n 3. 600mb \n 4. 800mb";
+      let myDatas = parseInt(prompt(myData));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    } else if (myAmount > 500) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 2) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 3) {
+    let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+    let myData2s = parseInt(prompt(myData2));
+    if (myData2s = Number) {
+      let final = confirm("Are you sure you want to complete this subscription");
+      alert("Successful Subscription, Thanks!");
+    }
+  } else if (myAccounts === 4) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  }
+} else if (myNetworks === 3) {
+  let myAccount = "1. Airtime-self \n 2. Airtime-others \n 3. Data \n 4. Transfer";
+  let myAccounts = parseInt(prompt(myAccount));
+  if (myAccounts === 1) {
+    let myAmount = prompt("Enter Amount");
+    if (myAmount <= 500) {
+      let myData = " 1. 200mb \n 2. 400mb \n 3. 600mb \n 4. 800mb";
+      let myDatas = parseInt(prompt(myData));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    } else if (myAmount > 500) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 2) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 3) {
+    let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+    let myData2s = parseInt(prompt(myData2));
+    if (myData2s = Number) {
+      let final = confirm("Are you sure you want to complete this subscription");
+      alert("Successful Subscription, Thanks!");
+    }
+  } else if (myAccounts === 4) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  }
+} else if (myNetworks === 4) {
+  let myAccount = "1. Airtime-self \n 2. Airtime-others \n 3. Data \n 4. Transfer";
+  let myAccounts = parseInt(prompt(myAccount));
+  if (myAccounts === 1) {
+    let myAmount = prompt("Enter Amount");
+    if (myAmount <= 500) {
+      let myData = " 1. 200mb \n 2. 400mb \n 3. 600mb \n 4. 800mb";
+      let myDatas = parseInt(prompt(myData));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    } else if (myAmount > 500) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 2) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  } else if (myAccounts === 3) {
+    let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+    let myData2s = parseInt(prompt(myData2));
+    if (myData2s = Number) {
+      let final = confirm("Are you sure you want to complete this subscription");
+      alert("Successful Subscription, Thanks!");
+    }
+  } else if (myAccounts === 4) {
+    let myMobile = prompt("Enter receiver mobile number");
+    if (myMobile >= 1) {
+      let myData2 = " 1. 1gb \n 2. 1.5gb \n 3. 2gb \n 4. 3gb";
+      let myData2s = parseInt(prompt(myData2));
+      if (myData2s = Number) {
+        let final = confirm("Are you sure you want to complete this subscription");
+        alert("Successful Subscription, Thanks!");
+      }
+    }
+  }
 }
