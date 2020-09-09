60cfa21a7a949ed36301d89d0aa9f59c16ba285d
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2020-09-09 11:29:46 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// DAY 2
+// PROMPT VALUE
+/*let myName = prompt("Enter your name");
+
+let myNames = parseInt(myName);
+alert(typeof (myNames));
+
+if (myNames >= 100) {
+  alert("century or above a century");
+}
+
+let myInput = prompt("Enter a value");
+
+alert(myInput); */
+
+// DAY 3
+// IF-ELSE STATEMENT
+// AIRTIME-SELF, AIRTIME-OTHERS, DATA, TRANSFER
+let myAccount = "Welcome to Lexitar Services Limited \n 1. Airtime-self \n 2. Airtime-others \n 3. Data \n 4. Transfer";
+variable = parseInt(prompt(myAccount));
+console.log(typeof (variable));
+/*let amount = "Amount \n 1. 100 \n 2. 200 \n 3. 300";
+prompt(amount);
+let ans = confirm("Are you sure?");*/
+
+if (variable === 1) {
+  prompt("Javascript is good");
+} else {
+
+}
