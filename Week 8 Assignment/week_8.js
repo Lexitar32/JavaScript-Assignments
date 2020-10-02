33656bae31ad2c17ed40fa1c166c9d9234b7e541
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2020-10-03 00:21:31 +0100

    committed

--- Week 8 Assignment/week_8.js
+++ Week 8 Assignment/week_8.js
@@ -4,7 +4,7 @@
 
 
 // Declaring Objects
-internalUsers = [
+allUsers = [
   {
     fullName: 'Arimoro Olamilekan',
     email: 'arimoroolamilekan1@gmail.com',
@@ -19,23 +19,23 @@ internalUsers = [
   }
 ];
 
-internalUsers[internalUsers.length] = {
+allUsers[allUsers.length] = {
   fullName: 'Malik Mukhtar',
   email: 'malikmukhtar@gmail.com',
   phone: '09077499020',
   pics: 'tie-1.jpg'
 }
 
-internalUsers[internalUsers.length] = {
+allUsers[allUsers.length] = {
   fullName: 'Oyetunji Atilade',
   email: 'oyetunjiatilade@gmail.com',
   phone: '07082158660',
   pics: 'tie.jpg'
 }
 
-internalUsers = JSON.parse(localStorage.getItem('storeUsers'));
-if (internalUsers == null) {
-  internalUsers = [];
+allUsers = JSON.parse(localStorage.getItem('storeUsers'));
+if (allUsers == null) {
+  allUsers = [];
 }
 
 // Contents to display
@@ -43,18 +43,18 @@ const mainDisplay = () => {
 
   user = '';
 
-  for (let i = 0; i < internalUsers.length; i++) {
+  for (let i = 0; i < allUsers.length; i++) {
     user += `<div>
-    <img src="${internalUsers[i].pics}" style="max-height:300px;"> <br>
-    <strong>Full Name</strong> : ${internalUsers[i].fullName} <br>
-    <strong>Email Address</strong> : ${internalUsers[i].email} <br>
-    <strong>Phone Number</strong> : ${internalUsers[i].phone} <br>
+    <img src="${allUsers[i].pics}" style="max-height:300px;"> <br>
+    <strong>Full Name</strong> : ${allUsers[i].fullName} <br>
+    <strong>Email Address</strong> : ${allUsers[i].email} <br>
+    <strong>Phone Number</strong> : ${allUsers[i].phone} <br>
     <button onClick="editUser(${i})">Edit</button>
     <button onClick="deleteUser(${i})">Delete</button>
   </div>`
   }
 
-  localStorage.setItem('storeUsers', JSON.stringify(internalUsers));
+  // localStorage.setItem('storeUsers', JSON.stringify(allUsers));
 
   document.getElementById('myUsers').innerHTML = user;
 }
@@ -67,9 +67,9 @@ const addUser = () => {
     phone: prompt('Enter your mobile number'),
     pics: 'tie.jpg'
   }
-  internalUsers.push(newUser);
+  allUsers.push(newUser);
 
-  localStorage.setItem('storeUsers', JSON.stringify(internalUsers));
+  localStorage.setItem('storeUsers', JSON.stringify(allUsers));
 
   mainDisplay();
 }
@@ -79,42 +79,42 @@ document.getElementById('add-user').addEventListener('click', addUser);
 
 // Delete User
 const deleteUser = (id) => {
-  if (confirm(`Are you sure you want to delete ${internalUsers[id].fullName}`)) {
-    internalUsers.splice(id, 1);
+  if (confirm(`Are you sure you want to delete ${allUsers[id].fullName}`)) {
+    allUsers.splice(id, 1);
 
     mainDisplay();
   } else {
     return alert('Be sure of the user you want to delete before doing so!');
   }
 
-  localStorage.setItem('storeUsers', JSON.stringify(internalUsers));
+  localStorage.setItem('storeUsers', JSON.stringify(allUsers));
+
+  mainDisplay();
 }
 
 // Edit User
 const editUser = (id) => {
   let edittedUser = {
-    "fullName": prompt("Enter your full Name", internalUsers[id].fullName),
-    "email": prompt("Enter your email", internalUsers[id].email),
-    "phone": prompt("Enter your phone", internalUsers[id].phone),
-    "pics": prompt("upload profile pics", internalUsers[id].pics)
+    "fullName": prompt("Enter your full Name", allUsers[id].fullName),
+    "email": prompt("Enter your email", allUsers[id].email),
+    "phone": prompt("Enter your phone", allUsers[id].phone),
+    "pics": prompt("upload profile pics", allUsers[id].pics)
   }
-  internalUsers[id] = edittedUser;
+  allUsers[id] = edittedUser;
 
-  localStorage.setItem('storeUsers', JSON.stringify(internalUsers));
+  localStorage.setItem('storeUsers', JSON.stringify(allUsers));
 
   mainDisplay();
 }
 
 function search() {
   param = document.getElementById('searchItems').value;
-  usersObject = internalUsers.find(x => x.fullName === param);
+  usersObject = allUsers.find(x => x.fullName === param);
   if (usersObject == undefined || usersObject == null) {
     alert(`No record found for ${param}`);
   } else {
-    internalUsers = [];
-    internalUsers.push(usersObject);
-
-    localStorage.getItem('storeUsers', JSON.stringify(internalUsers));
+    allUsers = [];
+    allUsers.push(usersObject);
 
     mainDisplay();
   }
@@ -122,13 +122,11 @@ function search() {
 
 function searchAll() {
   param = document.getElementById('searchItems').value;
-  usersObject = internalUsers.filter(x => x.fullName === param);
+  usersObject = allUsers.filter(x => x.fullName === param);
   if (usersObject == undefined || usersObject == null) {
     alert(`No record found for ${param}`);
   } else {
-    document.getElementById('recordFound').innerHTML = internalUsers.length + " records Found";
-
-    localStorage.getItem('storeUsers', JSON.stringify(internalUsers));
+    document.getElementById('recordFound').innerHTML = allUsers.length + " records Found";
 
     mainDisplay();
   }
@@ -136,19 +134,19 @@ function searchAll() {
 
 function searchPartMatch() {
   param = document.getElementById('searchItems').value;
-  internalUsers = internalUsers.filter(x => x.fullName.includes(param));
-  if (internalUsers == undefined || internalUsers == null) {
+  allUsers = allUsers.filter(x => x.fullName.includes(param));
+  if (allUsers == undefined || allUsers == null) {
     alert(`No record found for ${param}`);
   } else {
-    // internalUsers = [];
-    // internalUsers.push(usersObject);
-    document.getElementById('recordFound').innerHTML = internalUsers.length + " records Found";
-
-    localStorage.getItem('storeUsers', JSON.stringify(internalUsers));
+    document.getElementById('recordFound').innerHTML = allUsers.length + " records Found";
 
     mainDisplay();
   }
+
+  // allUsers = [];
+  // allUsers.push(usersObject);
 }
+
 mainDisplay();
 
 // const myUser =
